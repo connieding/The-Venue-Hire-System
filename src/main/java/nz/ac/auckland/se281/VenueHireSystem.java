ff183e80d4e77f5d536244c9df5622643189a36d
Connie Ding <cdin152@aucklanduni.ac.nz>
2024-03-20 23:56:36 +1300

    Fixed printing error in printVenues to print expected output when there are no venues

--- src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
+++ src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
@@ -5,10 +5,13 @@ import nz.ac.auckland.se281.Types.FloralType;
 
 public class VenueHireSystem {
 
-  public VenueHireSystem() {}
+  public VenueHireSystem() { 
+  }
 
   public void printVenues() {
     // TODO implement this method
+    System.out.println("There are no venues in the system. Please create a venue first.");
+    
   }
 
   public void createVenue(
